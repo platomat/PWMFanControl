95638fac0ef88adbd57b7b3d63cbf9105f375ec4
Wasilij <87741010+platomat@users.noreply.github.com>
2025-09-04 20:59:52 +0200

    Update FanProportional.py
    this scripts writes permanently a fan_status.json, so this data could be used to read it and to display some of its data in node-red or somewhere else

--- FanProportional.py
+++ FanProportional.py
@@ -4,40 +4,80 @@
 # Works well with a Pi Desktop Case with OLED Stats Display
 # Installation & Setup Instructions - https://www.the-diy-life.com/connecting-a-pwm-fan-to-a-raspberry-pi/
 
-import RPi.GPIO as IO          # Calling GPIO to allow use of the GPIO pins
-import time                    # Calling time to allow delays to be used
-import subprocess              # Calling subprocess to get the CPU temperature
+# EDITED BY: platomat.com
+# this scripts writes permanently a fan_status.json file that looks like this:
+# {"temp_current": 54.5, "temp_min": 45, "temp_max": 80, "fan_speed_current": 27, "fan_speed_min": 0, "fan_speed_max": 100}
 
-IO.setwarnings(False)          # Do not show any GPIO warnings
-IO.setmode (IO.BCM)            # BCM pin numbers - PIN8 as ‘GPIO14’
-IO.setup(14,IO.OUT)            # Initialize GPIO14 as our fan output pin
-fan = IO.PWM(14,100)           # Set GPIO14 as a PWM output, with 100Hz frequency (this should match your fans specified PWM frequency)
-fan.start(0)                   # Generate a PWM signal with a 0% duty cycle (fan off)
+# so this data could be used to read it and to display some of its data in node-red or somewhere else
 
-minTemp = 25                   # Temperature and speed range variables, edit these to adjust max and min temperatures and speeds
+
+import RPi.GPIO as IO
+import time
+import subprocess
+import json
+import os
+
+# --- Fan setup ---
+pwmGpio = 14            # GPIO number
+pwmFrequenzy = 25       # Hz
+updateInterval = 3      # seconds
+
+IO.setwarnings(False)
+IO.setmode(IO.BCM)
+IO.setup(pwmGpio, IO.OUT)
+fan = IO.PWM(pwmGpio, pwmFrequenzy)
+fan.start(0)
+
+# --- Config ---
+minTemp = 48
 maxTemp = 80
 minSpeed = 0
 maxSpeed = 100
 
-def get_temp():                             # Function to read in the CPU temperature and return it as a float in degrees celcius
+# JSON output file
+status_file = "fan_status.json"
+
+def get_temp():
+    """Read CPU temperature in °C as float"""
     output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
     temp_str = output.stdout.decode()
     try:
         return float(temp_str.split('=')[1].split('\'')[0])
     except (IndexError, ValueError):
         raise RuntimeError('Could not get temperature')
-    
-def renormalize(n, range1, range2):         # Function to scale the read temperature to the fan speed range
+
+def renormalize(n, range1, range2):
+    """Scale number from range1 to range2"""
     delta1 = range1[1] - range1[0]
     delta2 = range2[1] - range2[0]
     return (delta2 * (n - range1[0]) / delta1) + range2[0]
 
-while 1:                                    # Execute loop forever
-    temp = get_temp()                       # Get the current CPU temperature
-    if temp < minTemp:                      # Constrain temperature to set range limits
+while True:
+    # read temp and clamp
+    temp = get_temp()
+    if temp < minTemp:
         temp = minTemp
     elif temp > maxTemp:
         temp = maxTemp
-    temp = int(renormalize(temp, [minTemp, maxTemp], [minSpeed, maxSpeed]))
-    fan.ChangeDutyCycle(temp)               # Set fan duty based on temperature, from minSpeed to maxSpeed
-    time.sleep(5)                           # Sleep for 5 seconds
+
+    # map to fan speed
+    speed = int(renormalize(temp, [minTemp, maxTemp], [minSpeed, maxSpeed]))
+    fan.ChangeDutyCycle(speed)
+
+    # build status dict
+    status = {
+        "temp_current": round(temp, 1),
+        "temp_min": minTemp,
+        "temp_max": maxTemp,
+        "fan_speed_current": speed,
+        "fan_speed_min": minSpeed,
+        "fan_speed_max": maxSpeed
+    }
+
+    # write to file (atomic update)
+    tmp_file = status_file + ".tmp"
+    with open(tmp_file, "w") as f:
+        json.dump(status, f)
+    os.replace(tmp_file, status_file)
+
+    time.sleep(updateInterval)
